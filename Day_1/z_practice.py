11484e945a753cccaf67e49403a775447a2be01e
soewin23 <soewin2307@gmail.com>
2025-07-19 11:28:05 +0630

    practice question from ai

---
+++ Day_1/z_practice.py
@@ -0,0 +1,23 @@
+
+# Print your name, age, and favorite color.
+print("Soe Win")
+print(17)
+print("Yellow")
+
+# Create variables for name, age, and hobby — print them in one sentence.
+name = "Soe Win"
+age = 17
+hobby = "coding"
+
+
+
+# Write a comment in your code that explains what the program does.
+
+
+print(name, age, hobby)# Soe Win, 17, coding
+
+
+
+#using f""
+
+print(f"My name is {name}. I'm {age} years old. My hobby is {hobby}.")
