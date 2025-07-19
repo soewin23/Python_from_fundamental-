7db6e77414c2d702bd3e65ee8201f5d67c7b7b28
soewin23 <soewin2307@gmail.com>
2025-07-19 12:58:51 +0630

    Day 3 of python

---
+++ Day_3/ifelseif.py
@@ -0,0 +1,26 @@
+
+print("Grading system")
+
+m = int(input("Enter your makrs: "))
+if m > 39 and m== 59:
+    print("Your grade is C")
+elif m > 59:
+    print("your grade is B")
+elif m >79:
+    print("your grade is A")
+elif m > 100:
+    print("Error marks")
+else:
+    print("Fail")
+
+
+
+
+
+
+
+age = int(input("Enter your age "))
+if age >= 18:
+    print("You can dive a car")
+else:
+    print("You can't drive a car")
