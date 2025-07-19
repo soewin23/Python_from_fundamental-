b9de07fe92ee8a2246bb608d79b0cd6df80f143f
soewin23 <soewin2307@gmail.com>
2025-07-19 12:31:24 +0630

    Day_2_of python

---
+++ Day_2/input&output.py
@@ -0,0 +1,16 @@
+#       we can give intput using input 
+
+
+name = input("Enter your name ")
+print(f'hello {name}')
+
+
+age = int(input("Entr your age :"))
+print(f"Yoy are {age } years old.")
+if age >= 18:
+    print(f"{age}, you are an Adult")
+else:
+    print(f"{age}, You are a kid.")
+
+Gender  = input("Where your sex :")  
+print(f"You are {Gender}")
