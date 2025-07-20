6f3515b6841013700596e199d957ff2236310f40
soewin23 <soewin2307@gmail.com>
2025-07-20 13:17:50 +0630

    Day_4_of_Python

---
+++ Day_4/PythonList.py
@@ -0,0 +1,36 @@
+#       Pyhton lists
+
+# list are used to store multiple items in a single variable.
+
+# list are one of four built-in data type In python
+"""1 list 
+2 tuple
+3 Set 
+4 Dictionary"""
+
+
+"""Lists are created using square brackets:"""
+
+thisislist = ["apple","banana","apple" ,"orange", "mango"]
+print(type(thisislist))
+print(thisislist)
+
+# list items
+newlist = thisislist[0] # by using the square bracket we can acces the items in a list
+print(newlist)
+nlist = thisislist[1]
+print(nlist)
+llist = thisislist[2]
+print(llist)
+
+# List Length
+
+
+# To determine how many items a list has, use the len() function:
+new = ["apple","banana","apple" ,"orange", "mango"]
+print(len(new))
+
+# A list can contain different data types:
+
+n =["new", 17, True, 12.23]
+
