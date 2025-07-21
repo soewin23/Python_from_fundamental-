73aac1cf6520090c4a8c59f9ed576d80df9b8066
soewin23 <soewin2307@gmail.com>
2025-07-21 23:56:06 +0630

    Day_6_Of_Python

---
+++ Day_6_Turple/pythonTuple.py
@@ -0,0 +1,23 @@
+# Tuples are used to store multiple items in a single variable.
+
+example = ("a","b","c","d","e","f")
+print(type(example))
+print(example)
+
+#Lenght of tuple
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+
+# Create Tuple With One Item
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thisnottuple = ("apple")
+print(type(thisnottuple))
+
+# The tuple() Constructor
+thisistuple = tuple(("apple","banan","cherry"))# note the double round-brackets
+print(thisistuple)
