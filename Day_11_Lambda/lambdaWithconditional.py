305d2931a3a1d6d709a91a859e1181710e9baa95
soewin23 <soewin2307@gmail.com>
2025-07-30 01:33:54 +0630

    Python_Learnings

--- Day_11_Lambda/lambdaWithconditional.py
+++
@@ -1,8 +0,0 @@
-#  (if-else)
-# we can use if-else inside a lambda!
-
-
-max_val = lambda a,b:a if a>b else b
-print(max_val(1,10))# 10
-
-
--- Day_7_Dict/03_Change_items.py
+++
@@ -1,7 +0,0 @@
-    # Change Values
-
-
-# Update the "name" of the car by using the update() method:
-Dict = {"name":"Soe Win", "Age":18,"year": 2007, "Male":True}
-Dict.update({"name":"Bimal"})#{'name': 'Bimal', 'Age': 18, 'year': 2007, 'Male': True}
-print(Dict)
